50d28c7d0762e492c4093a0d2ce19404b5f933d0
alimshare <alimm.abdullah@gmail.com>
2020-12-05 09:51:41 +0700

    fix conflict cache key

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -9,7 +9,7 @@ class PublicController extends Controller
 {
     public function halaqoh27()
     {
-        $this->data['list'] = Cache::remember('viewHalaqoh.all', 60*60*24*7, function () {
+        $this->data['list'] = Cache::remember('viewHalaqoh.27', 60*60*24*7, function () {
             return \App\Model\View\ViewPeserta::select('nis','santri_name','pengajar_name','program_name','day','gender')
                 ->where('semester_id', 12)
                 ->orderBy('santri_name','asc')
