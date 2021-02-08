fcf2922cfa280ced1718e1a3de59485577966c62
alimshare <alimm.abdullah@gmail.com>
2021-02-08 12:04:47 +0700

    add select2 to option on form tambah peserta halaqoh

--- resources/views/pages/halaqoh/form-tambah-peserta.blade.php
+++ resources/views/pages/halaqoh/form-tambah-peserta.blade.php
@@ -1,16 +1,5 @@
 @extends('layouts.materialized')
 
-@section('header-script')
-<style type="text/css">
-    table td, table th {
-        border : 1px solid #ddd;
-    }
-    .error {
-        color : red;
-    }
-</style>
-@endsection
-
 @section('content')
 
 <!--breadcrumbs start-->
@@ -41,27 +30,29 @@
               <form class="" action="{{ route('halaqoh.peserta.addPost') }}" method="POST" id="formValidate" autocomplete="off">
                  <div class="card-panel" id="profile-card">
                     @csrf
+
                     <div class="row">
-                       <div class="input-field col s12">
-                          <select name="halaqoh" id="halaqoh">
-                            @foreach($halaqoh as $h)
-                                <option value="{{ $h->halaqoh_id }}">{{ $h->pengajar_name. ' - ' . $h->program_name .' ('. $h->day . ')'   }}</option>
-                            @endforeach
-                          </select>
-                          <label for="halaqoh">Halaqoh Awal</label>
-                          <div id="halaqoh-error" class="error"></div>
-                       </div>
+                        <label for="halaqoh" class="col s12">Halaqoh</label>    
+                        <div class="input-field col s12">
+                           <select name="halaqoh" id="halaqoh" class="select2">
+                              @foreach($halaqoh as $h)
+                                 <option value="{{ $h->halaqoh_id }}">{{ $h->pengajar_name. ' - ' . $h->program_name .' ('. $h->day . ')'   }}</option>
+                              @endforeach
+                           </select>
+                           <div id="halaqoh-error" class="error"></div>
+                        </div>
                     </div>
-                    <div class="row">
-                       <div class="input-field col s12">
-                          <select name="santri" id="santri">
-                            @foreach($peserta as $santri)
-                                <option value="{{ $santri->id }}">{{ $santri->name . ' - ' .$santri->nis  }}</option>
-                            @endforeach
-                          </select>
-                          <label for="santri">Peserta</label>
-                          <div id="santri-error" class="error"></div>
-                       </div>
+
+                    <div class="row" style="margin-bottom:2em; margin-top:1em">
+                        <label for="santri" class="col s12">Peserta</label>    
+                        <div class="input-field col s12">
+                           <select name="santri" id="santri" class="select2">
+                              @foreach($peserta as $santri)
+                                 <option value="{{ $santri->id }}">{{ $santri->name . ' - ' .$santri->nis  }}</option>
+                              @endforeach
+                           </select>
+                           <div id="santri-error" class="error"></div>
+                        </div>
                     </div>
 
                     <div class="card-footer text-right" style="margin-top: 1rem;">                                           
@@ -80,22 +71,25 @@
        
 @endsection
 
+@section('header-script')
+<style type="text/css">
+    table td, table th {
+        border : 1px solid #ddd;
+    }
+    .error {
+        color : red;
+    }
+</style>
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+@endsection
+
 @section('footer-script')
-    <script>
-        document.getElementById("cek").addEventListener("click", cek);
-
-        function cek()
-        {
-            let halaqohId = document.getElementById('old_halaqoh').value;
-            let elemPeserta = document.getElementById('peserta');
-
-            if (elemPeserta == null) {
-                document.location = '/halaqoh/pindah/'+halaqohId;
-            } else {
-                let pesertaId = elemPeserta.value;
-                document.location = '/halaqoh/pindah/'+halaqohId+'/'+pesertaId;
-
-            }
-        }
-    </script>
+   <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+   <script>
+      // Basic Select2 select
+      $(".select2").select2({
+         dropdownAutoWidth: true,
+         width: '100%'
+      });
+   </script>
 @endsection
