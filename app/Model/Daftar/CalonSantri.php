f8e120c2bad42bb7d278ae634958fb61ddd46200
Abdullah 'Alim (alimshare) <alimm.abdullah@gmail.com>
2025-07-11 09:44:02 +0700

    fix: calon santri using softDelete

--- app/Model/Daftar/CalonSantri.php
+++ app/Model/Daftar/CalonSantri.php
@@ -3,9 +3,12 @@
 namespace App\Model\Daftar;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CalonSantri extends Model
 {
+    use SoftDeletes;
+    
     protected $connection = 'mysql_daftar';
     protected $table = "calon_santri"; 
     
