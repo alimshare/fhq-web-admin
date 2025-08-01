718bd02af0a5db85e70a67a045f2e57f2a90c195
Abdullah 'Alim (alimshare) <alimm.abdullah@gmail.com>
2025-08-01 12:57:09 +0700

    add badge kbm on manage_v2

--- resources/views/pages/halaqoh/manage-v2.blade.php
+++ resources/views/pages/halaqoh/manage-v2.blade.php
@@ -213,10 +213,10 @@
                                                 <a class="btn-floating activator btn-move-up waves-effect waves-light red accent-2 z-depth-4 right" href="/halaqoh/{{ $h->reference }}">
                                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28"><title>magnify</title><path fill="white" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" /></svg>
                                                 </a>
-
-                                                @if(strtoupper($h->jenis_kbm) == "ONLINE") <span class="badge new">Online</span> @endif
                                                 
                                             @endallow
+                                            
+                                            @if(strtoupper($h->jenis_kbm) == "ONLINE") <span class="badge new">Online</span> @endif
                                         </div>
 
                                         <ul class="collection program-container-body" id="collection-{{ $h->reference }}" data-ref="{{ $h->reference }}" style="width:100%;height:270px;overflow-y:scroll">
